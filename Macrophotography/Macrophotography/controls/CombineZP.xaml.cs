312f577694784c6dc64cfa1f8241188ac0c24f0e
arpinerap <arpinerap@gmail.com>
2016-03-09 00:42:31 +0100

    CombineZP Tab Control

---
+++ Macrophotography/Macrophotography/controls/CombineZP.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Threading;
+using Macrophotography.controls;
+using Macrophotography;
+using CameraControl.Core;
+using CameraControl.Core.Classes;
+using CameraControl.Devices;
+
+namespace Macrophotography.controls
+{
+    /// <summary>
+    /// Interaction logic for CombineZP.xaml
+    /// </summary>
+    public partial class CombineZP : UserControl
+    {
+        public CombineZP()
+        {
+            InitializeComponent();
+        }
+    }
+}
